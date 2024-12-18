7bc0c26fdaa007d3bbcbdcef99d64ad865132e09
Jainamroxz <yoginum2@outlook.com>
2024-12-18 13:56:37 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7236650621:AAG0A6lMX_xMtlkfzxZHWJQt68050rN4o68")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("God_x_Sikandar")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1216702307")
+
+
+
+
+
+
+
+  
