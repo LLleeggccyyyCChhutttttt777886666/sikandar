46673b0691a93eb996d2be28e0f50707354c19f9
Legacychutt <yoginum2@outlook.com>
2024-12-29 03:22:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7763566676:AAFPUFSFHkdnPppDy5ov6tumAsWuTcgltUU")
+BOT_TOKEN=("7507854182:AAEstZOWgVGB4AgU-33fbcI2w0DdphFRy3s")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@Mr_sid_001")
+OWNER_USERNAME=("@God_x_Sikandar")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1329951770")
+ADMIN_IDS=("1216702307")
 
 
 
